f7d0fc0ce329430c6e488475406539c8e255d4ed
zNadir <115375151+znadir@users.noreply.github.com>
2024-08-28 17:28:26 -0400

    mobile navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ import {
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import SearchIcon from "@mui/icons-material/Search";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -36,11 +37,59 @@ export default function RootLayout({
 			<body className={inter.className}>
 				<header>
 					<AppBar position='sticky' color='default'>
-						<Container maxWidth='xl' sx={{ my: 2 }}>
-							<Box sx={{ display: "flex", justifyContent: "space-between", width: "100%" }}>
-								<Box sx={{ display: "flex", flex: 1 }}>
-									<Image src='/logo.svg' alt='Eco Essentials' width={179} height={44.15} />
+						<Container maxWidth='xl' sx={{ my: { xs: 1, md: 2 } }}>
+							{/* Mobile NavBar */}
+							<Box sx={{ display: { xs: "block", md: "none" } }}>
+								<Box
+									sx={{
+										display: "flex",
+										justifyContent: "space-between",
+										width: "100%",
+									}}
+								>
+									<Link href='/' style={{ display: "block" }}>
+										<Image src='/logo.svg' alt='Eco Essentials' width={179} height={44.15} />
+									</Link>
 
+									<Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+										<IconButton>
+											<AccountCircleIcon />
+										</IconButton>
+										<IconButton>
+											<ShoppingCartIcon />
+										</IconButton>
+									</Box>
+								</Box>
+
+								<Paper
+									component='form'
+									sx={{
+										p: "0px 4px",
+										display: "flex",
+										alignItems: "center",
+										width: "100%",
+										borderRadius: 2,
+									}}
+								>
+									<InputBase sx={{ ml: 1, flex: 1 }} placeholder='Search Eco Essentials' />
+									<IconButton type='button' sx={{ p: "10px" }} aria-label='search'>
+										<SearchIcon />
+									</IconButton>
+								</Paper>
+							</Box>
+
+							{/* Desktop NavBar */}
+							<Box
+								sx={{
+									display: { xs: "none", md: "flex" },
+									justifyContent: "space-between",
+									width: "100%",
+								}}
+							>
+								<Box sx={{ display: "flex", flex: 1 }}>
+									<Link href='/' style={{ display: "block" }}>
+										<Image src='/logo.svg' alt='Eco Essentials' width={179} height={44.15} />
+									</Link>
 									<Paper
 										component='form'
 										sx={{
