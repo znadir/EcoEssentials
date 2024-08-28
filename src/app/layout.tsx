19ebbb85d602d3d30427d829eb44a675c30774ba
zNadir <115375151+znadir@users.noreply.github.com>
2024-08-28 17:06:07 -0400

    add hero banner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,45 +36,43 @@ export default function RootLayout({
 			<body className={inter.className}>
 				<header>
 					<AppBar position='sticky' color='default'>
-						<Container maxWidth='xl' sx={{ my: 1 }}>
-							<Toolbar>
-								<Box sx={{ display: "flex", justifyContent: "space-between", width: "100%" }}>
-									<Box sx={{ display: "flex", flex: 1 }}>
-										<Image src='/logo.svg' alt='Eco Essentials' width={179} height={44.15} />
+						<Container maxWidth='xl' sx={{ my: 2 }}>
+							<Box sx={{ display: "flex", justifyContent: "space-between", width: "100%" }}>
+								<Box sx={{ display: "flex", flex: 1 }}>
+									<Image src='/logo.svg' alt='Eco Essentials' width={179} height={44.15} />
 
-										<Paper
-											component='form'
-											sx={{
-												p: "0px 4px",
-												display: "flex",
-												alignItems: "center",
-												width: "100%",
-												mx: 3,
-												borderRadius: 2,
-											}}
-										>
-											<InputBase sx={{ ml: 1, flex: 1 }} placeholder='Search Eco Essentials' />
-											<IconButton type='button' sx={{ p: "10px" }} aria-label='search'>
-												<SearchIcon />
-											</IconButton>
-										</Paper>
-									</Box>
-
-									<Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
-										<Button variant='contained' size='medium' startIcon={<AccountCircleIcon />}>
-											Account
-										</Button>
-										<Button
-											variant='contained'
-											size='medium'
-											color='secondary'
-											startIcon={<ShoppingCartIcon />}
-										>
-											Cart
-										</Button>
-									</Box>
+									<Paper
+										component='form'
+										sx={{
+											p: "0px 4px",
+											display: "flex",
+											alignItems: "center",
+											width: "100%",
+											mx: 3,
+											borderRadius: 2,
+										}}
+									>
+										<InputBase sx={{ ml: 1, flex: 1 }} placeholder='Search Eco Essentials' />
+										<IconButton type='button' sx={{ p: "10px" }} aria-label='search'>
+											<SearchIcon />
+										</IconButton>
+									</Paper>
 								</Box>
-							</Toolbar>
+
+								<Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+									<Button variant='contained' size='medium' startIcon={<AccountCircleIcon />}>
+										Account
+									</Button>
+									<Button
+										variant='contained'
+										size='medium'
+										color='secondary'
+										startIcon={<ShoppingCartIcon />}
+									>
+										Cart
+									</Button>
+								</Box>
+							</Box>
 						</Container>
 					</AppBar>
 				</header>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,33 @@
-import { Button } from "@mui/material";
+import { Box, Button, Container } from "@mui/material";
+import Image from "next/image";
 
 export default function Home() {
 	return (
 		<main>
-			<h1>Home</h1>
-			<Button variant='text'>Text</Button>
+			<Container maxWidth='xl' sx={{ my: 1 }}>
+				<Box sx={{ display: { xs: "none", md: "block" } }}>
+					<Image
+						style={{ width: "100%", height: "100%" }}
+						src='/deskbanner.png'
+						alt='Buy Eco-friendly products'
+						width={1427}
+						height={235}
+					/>
+				</Box>
+
+				<Box sx={{ display: { xs: "block", md: "none" } }}>
+					<Image
+						style={{ width: "100%", height: "100%" }}
+						src='/mobbanner.png'
+						alt='Buy Eco-friendly products'
+						width={812}
+						height={287}
+					/>
+				</Box>
+
+				<h1>Home</h1>
+				<Button variant='text'>Text</Button>
+			</Container>
 		</main>
 	);
 }
